59512037ec3da8dcd960155ca258b8100aa16ebc
Uxcfg <aga.@gmail.com>
2020-02-16 19:00:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+
+var leftBorderWidth = 5;
+let second = 2;
+console.log(leftBorderWidth);
+
+/* let persone = {
+    name: "Ivan",
+    age: 23,
+    isMarried: false
+}
+
+console.log(persone.isMarried); */
+
+/* let answer = prompt ("Есть ли вам 18?", "da");
+console.log(typeof(answer));
+
+console.log("- arr"+ "- object"); */
+
+let a = true,
+    b = false;
+
+console.log(a && b);
+
